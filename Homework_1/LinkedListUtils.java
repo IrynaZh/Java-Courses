a72061dcd2b2554142d1bcc508a62bec5324008d
Iryna <iryna.zh2504@gmail.com>
2019-09-02 11:29:22 -0400

    LinkedLists
    LinkedLists

---
+++ Homework_1/LinkedListUtils.java
@@ -0,0 +1,44 @@
+
+
+import java.util.LinkedList;
+
+/*
+ * SD2x Homework #1
+ * Implement the methods below according to the specification in the assignment description.
+ * Please be sure not to change the signature of any of the methods!
+ */
+
+public class LinkedListUtils {
+	
+	public static void insertSorted(LinkedList<Integer> list, int value) {
+		if (list != null) {
+			int index = 0;
+			while (index < list.size()) {
+				if (list.get(index) < value ) {
+					index++
+				}
+				else {
+					break;
+				}
+			}
+			list.add(index,value)
+		}
+
+		/* IMPLEMENT THIS METHOD! */
+
+	}
+	
+
+	public static void removeMaximumValues(LinkedList<String> list, int N) {
+
+		/* IMPLEMENT THIS METHOD! */
+
+	}
+	
+	public static boolean containsSubsequence(LinkedList<Integer> one, LinkedList<Integer> two) {
+
+		/* IMPLEMENT THIS METHOD! */
+		
+		return true; // this line is here only so this code will compile if you don't modify it
+	}
+}
